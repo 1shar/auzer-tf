589d3bcacda829bd9bbb0f94e65451afd8e0e896
1shar <kiwioo39@gmail.com>
2023-01-27 11:11:19 +0900

    Update main.tf

--- sql-vm/main.tf
+++ sql-vm/main.tf
@@ -180,7 +180,7 @@ resource "azurerm_mssql_virtual_machine" "mssqlvm" {
   }
  
  storage_configuration {
-    disk_type             = "NEW"
+    disk_type             = "EXTEND"
     storage_workload_type = "GENERAL"
     data_settings {
       default_file_path = "E:\\Data"
