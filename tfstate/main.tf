2c2dc2b675877bec1954531500f9cddc52a09f17
1shar <kiwioo39@gmail.com>
2023-06-09 13:06:22 +0900

    Update main.tf

--- tfstate/main.tf
+++ tfstate/main.tf
@@ -9,21 +9,21 @@ resource "azurerm_resource_group" "tfstate" {
 
 resource "azurerm_storage_account" "tfstate" {
   name                     = "${var.company}${var.system}tfstorage"
-  resource_group_name      = "${local.company}-${local.system}-tfstate-rg"
+  resource_group_name      = "${var.company}-${var.system}-tfstate-rg"
   location                 = azurerm_resource_group.tfstate.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
  # allow_blob_public_access = true
 
   tags = {
-    company = local.company
-    service = local.system
-    location = local.location
+    company = var.company
+    service = var.system
+    location = var.location
     managedby = "iac"
 }
 }
 resource "azurerm_storage_container" "tfstate" {
-  name                  = "${local.company}${local.system}tfstate"
+  name                  = "${var.company}${var.system}tfstate"
   storage_account_name  = azurerm_storage_account.tfstate.name
   container_access_type = "blob"
 }
