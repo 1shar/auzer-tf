c393bd2f2ea51792c7c9857040916aaf8520e176
1shar <kiwioo39@gmail.com>
2023-06-12 07:52:23 +0900

    Update main.tf

--- tfstate/main.tf
+++ tfstate/main.tf
@@ -9,7 +9,7 @@ resource "azurerm_resource_group" "tfstate" {
 
 resource "azurerm_storage_account" "tfstate" {
   name                     = "${var.company}${var.system}tfstorage"
-  resource_group_name      = "${var.company}-${var.system}-tfstate-rg"
+  resource_group_name      = azurerm_resource_group.tfstate.name
   location                 = azurerm_resource_group.tfstate.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
