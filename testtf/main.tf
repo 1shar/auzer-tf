ce17e8da7531b87902160dfebd60eb67530f9f72
1shar <kiwioo39@gmail.com>
2023-06-14 14:52:24 +0900

    Create main.tf

---
+++ testtf/main.tf
@@ -0,0 +1,25 @@
+resource "azurerm_resource_group" "tfstate" {
+  name     = "${var.company}-${var.system}-rg-tfstate" # hws-esh-rg
+  location = var.location
+}
+
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "${var.company}${var.system}tfstorage"
+  resource_group_name      = azurerm_resource_group.tfstate.name
+  location                 = azurerm_resource_group.tfstate.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+ # allow_blob_public_access = true
+
+  tags = {
+    company = var.company
+    service = var.system
+    location = var.location
+    managedby = "iac"
+}
+}
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "${var.company}${var.system}tfstate"
+  storage_account_name  = azurerm_storage_account.tfstate.name
+  container_access_type = "blob"
+}
