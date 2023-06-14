e8f0a077f6714ff495636e83ae45450f36415461
1shar <kiwioo39@gmail.com>
2023-06-14 14:52:47 +0900

    Create variables.tf

---
+++ testtf/variables.tf
@@ -0,0 +1,30 @@
+variable "service" {
+  description = "service type"
+  type        = string
+  default     = "test"
+}
+
+variable "system" {
+  description = "service type"
+  type        = string
+  default     = "test"
+}
+
+
+variable "environment" {
+  description = "service environment"
+  type        = string
+  default     = "test"
+}
+
+variable "company" {
+  description = "company name"
+  type        = string
+  default     = "company"
+}
+
+variable "location" {
+  description = "Azure location"
+  type        = string
+  default     = "koreacentral"
+}
